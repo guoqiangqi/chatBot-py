da8c6b44dcf56236aa68c0507a4b34ea1d1e14a8
Guoqiang QI <guoqiang.qi1@gmail.com>
2023-03-06 07:22:27 +0000

    Calculate time spent on gpt-3.5 request.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 # Note: you need to be using OpenAI Python v0.27.0 for the code below to work
 import openai
 import pandas as pd
+import time
 
-sourceFile = "./data/data_schema.xlsx"
+count = 0
+timeTotal = 0
+sourceFile = "./data/openeuler_bot_backend_log.xlsx"
 
 if __name__ == "__main__":
-    reader = pd.read_excel(sourceFile, engine="openpyxl")
+    reader = pd.read_excel(sourceFile, sheet_name="valid_qa", engine="openpyxl")
     questiones = reader["用户输入"]
 
     messageSystem = {"role": "system", "content": "You are a openEuler community assistant, your name is Xiao Zhi."}
@@ -17,12 +20,16 @@ if __name__ == "__main__":
     tokens = []
     for question in questiones:
         messageQuestion = {"role": "user", "content": question}
-        messages = [messageSystem, messageQuestion]
+        messages = [messageQuestion]
 
+        count += 1
+        now = time.time()
         response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages= messages
                 )
+        end = time.time()
+        timeTotal = end - now + timeTotal
 
         token = response["usage"]
         tokens.append(token["total_tokens"])
@@ -32,4 +39,7 @@ if __name__ == "__main__":
     reader["answerChatGPT"] = answers
     reader["tokenUsage"] = tokens
 
-    reader.to_excel("./data/data_schema.xlsx", index=True)
+    print("QA finished: {} questiones have been processed, {} seconds have been spent.".format(count, timeTotal))
+    print("Writing back to excel....")
+    reader.to_excel("./data/openeuler_bot_backend_log_compare.xlsx", index=False)
+    print("Job finished!")
